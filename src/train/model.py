d443f676a3b31f6e4e77a74f973e0c50ccbbac48
LaoveHwx <1474141901@qq.com>
2026-03-12 14:51:53 +0800

    改为随模型类别数量自动生成对应模型，不用每次都改类别数量

--- src/train/model.py
+++ src/train/model.py
@@ -1,8 +1,61 @@
+# import tensorflow as tf
+# from tensorflow.keras import layers, models
+# 基于全连接层（Dense）的神经网络模型构建
+
+# def build_model(input_dim=42, num_classes=8):
+#
+#     model = models.Sequential([
+#
+#         layers.Input(shape=(input_dim,)),
+#
+#         layers.Dense(128),
+#         layers.BatchNormalization(),
+#         layers.Activation("relu"),
+#         layers.Dropout(0.3),
+#
+#         layers.Dense(64),
+#         layers.Activation("relu"),
+#
+#         layers.Dense(num_classes, activation="softmax")
+#
+#     ])
+#
+#     model.compile(
+#         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+#         loss="categorical_crossentropy",
+#         metrics=["accuracy"]
+#     )
+#
+#     return model
+
+
+# # 先训练两组测试可行性
+# def build_model(num_classes):
+#
+#     from tensorflow.keras.models import Sequential
+#     from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Activation
+#
+#     model = Sequential()
+#
+#     model.add(Dense(128, input_shape=(42,)))
+#     model.add(BatchNormalization())
+#     model.add(Activation("relu"))
+#     model.add(Dropout(0.3))
+#
+#     model.add(Dense(64))
+#     model.add(Activation("relu"))
+#
+#     model.add(Dense(num_classes, activation="softmax"))
+#
+#     return model
+
+
+# 不用改模型数量的跟随模型数量自适应版本
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-
-def build_model(input_dim=42, num_classes=8):
+# 基于全连接层（Dense）的神经网络模型构建
+def build_model(input_dim, num_classes):
 
     model = models.Sequential([
 
@@ -22,7 +75,7 @@ def build_model(input_dim=42, num_classes=8):
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-        loss="categorical_crossentropy",
+        loss="sparse_categorical_crossentropy",
         metrics=["accuracy"]
     )
 
--- src/train/train.py
+++ src/train/train.py
@@ -1,7 +1,6 @@
 # 训练脚本
 from data_loader import load_dataset
 from model import build_model
-import tensorflow as tf
 from pathlib import Path
 
 
@@ -10,9 +9,12 @@ def main():
     # 1 加载数据
     X_train, X_val, X_test, y_train, y_val, y_test, le = load_dataset()
 
-    # 2 构建模型
-    model = build_model()
+# 自动确定维度
+    input_dim = X_train.shape[1]
+    num_classes = y_train.shape[1]
 
+    # 2 构建模型 自动确定传入build_model
+    model = build_model(input_dim, num_classes)
     model.summary()
 
     # 3 训练模型
@@ -27,7 +29,7 @@ def main():
     # 4 测试集评估
     test_loss, test_acc = model.evaluate(X_test, y_test)
 
-    print("Test Accuracy:", test_acc)
+    print("Test Accuracy测试准确率:", test_acc)
 
     # 5 保存模型
     PROJECT_ROOT = Path(__file__).resolve().parents[2]
@@ -35,10 +37,49 @@ def main():
 
     model_path.mkdir(exist_ok=True)
 
-    model.save(model_path / "gesture_model.h5")
+    model.save(model_path / "gesture_model.keras")
 
-    print("模型已保存")
+    print("模型已保存为gesture_model.keras")
 
 
 if __name__ == "__main__":
     main()
+
+# # 先测试小数据
+# from data_loader import load_dataset
+# from model import build_model
+#
+#
+# def main():
+#
+#     X_train, X_val, X_test, y_train, y_val, y_test, le = load_dataset()
+#
+#     num_classes = y_train.shape[1]
+#
+#     model = build_model(num_classes)
+#
+#     model.compile(
+#         optimizer="adam",
+#         loss="categorical_crossentropy",
+#         metrics=["accuracy"]
+#     )
+#
+#     model.summary()
+#
+#     history = model.fit(
+#         X_train,
+#         y_train,
+#         validation_data=(X_val, y_val),
+#         epochs=50,
+#         batch_size=32
+#     )
+#
+#     test_loss, test_acc = model.evaluate(X_test, y_test)
+#
+#     print("Test Accuracy:", test_acc)
+#
+#
+# if __name__ == "__main__":
+#     main()
+
+
