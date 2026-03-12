550f122bd43b366812b47756487ff5a841532a68
LaoveHwx <1474141901@qq.com>
2026-03-12 14:51:08 +0800

    改进为多线程处理数据

--- src/train/data_loader.py
+++ src/train/data_loader.py
@@ -3,29 +3,48 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
+# 多线程版本
+from concurrent.futures import ThreadPoolExecutor
+
+def load_single_file(args):
+    npy_file, label = args
+    data = np.load(npy_file)
+
+    if data.shape == (42,):
+        return data, label
+    return None
 
 
 def load_dataset():
 
-    # 定位项目根目录
     PROJECT_ROOT = Path(__file__).resolve().parents[2]
     data_path = PROJECT_ROOT / "data" / "keypoints"
 
-    X = []
-    y = []
+    tasks = []
 
-    # 遍历每个类别文件夹
+    # 收集所有文件任务
     for class_dir in data_path.iterdir():
         if class_dir.is_dir():
 
             label = class_dir.name
 
             for npy_file in class_dir.glob("*.npy"):
-                data = np.load(npy_file)
+                tasks.append((npy_file, label))
 
-                if data.shape == (42,):
-                    X.append(data)
-                    y.append(label)
+    print("全部文件:", len(tasks))
+
+    X = []
+    y = []
+
+    # 多线程读取
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        results = executor.map(load_single_file, tasks)
+
+    for r in results:
+        if r is not None:
+            data, label = r
+            X.append(data)
+            y.append(label)
 
     X = np.array(X)
     y = np.array(y)
@@ -37,38 +56,33 @@ def load_dataset():
     le = LabelEncoder()
     y_encoded = le.fit_transform(y)
 
-    # ===测试代码开始===
     print("标签映射:")
     for i, name in enumerate(le.classes_):
         print(i, name)
-    # ===测试代码结束===
 
-    # one-hot
-    # 将整数编码的标签转换为 one-hot 编码，用于多分类任务
     y_onehot = to_categorical(y_encoded)
 
-    # ===测试代码开始===
-    print("onehot example:")
-    print(y_onehot[:5])
-    # ===测试代码结束===
-
-    # 划分训练/测试
+    # 第一次划分
     X_train, X_temp, y_train, y_temp, y_train_label, y_temp_label = train_test_split(
-        X,y_onehot,y_encoded,
+        X,
+        y_onehot,
+        y_encoded,
         test_size=0.3,
         random_state=42,
         stratify=y_encoded
     )
 
+    # 第二次划分
     X_val, X_test, y_val, y_test = train_test_split(
-        X_temp,y_temp,
+        X_temp,
+        y_temp,
         test_size=0.5,
         random_state=42,
         stratify=y_temp_label
     )
 
-    print("训练集:", X_train.shape)
-    print("验证集:", X_val.shape)
-    print("测试集:", X_test.shape)
+    print("训练:", X_train.shape)
+    print("验证:", X_val.shape)
+    print("测试:", X_test.shape)
 
     return X_train, X_val, X_test, y_train, y_val, y_test, le
--- src/train/test_loder.py
+++ src/train/test_loder.py
@@ -1,4 +1,7 @@
+# 标签编码，划分训练集
 from data_loader import load_dataset
 
 if __name__ == "__main__":
     X_train, X_val, X_test, y_train, y_val, y_test, le = load_dataset()
+
+
